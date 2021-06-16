e23dc35ad98f841d64e9b1383ea84c7158cd46d5
Tid <285492845@qq.com>
2021-06-16 14:10:08 +0800

    首版

---
+++ MyCpro/MyCpro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyCpro
+//
+//  Created by Tid on 2021/6/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
